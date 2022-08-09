2649ad7e988c40cedba2e779ac9e7efef5b7ac97
koppper <89693408+koppper@users.noreply.github.com>
2022-08-09 19:28:59 +0600

    some changes

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from blog.views import PostList, DetailPost, CreatePost
 urlpatterns = [
     path('', PostList.as_view(), name='blog'),
     path('<int:pk>/', DetailPost.as_view(), name='blog_item'),
-    path('', CreatePost.as_view(), name='create_post'),
+    path('create_post/', CreatePost.as_view(), name='create_post'),
 ]
